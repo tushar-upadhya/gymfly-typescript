02422772eca4edce276faae73df9f6bf382baf2f
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-07-01 23:11:19 +0530

    former motion

--- src/components/benefit/Benefit.tsx
+++ src/components/benefit/Benefit.tsx
@@ -1,6 +1,12 @@
+import { motion } from "framer-motion";
 import { SelectedPage } from "../../common/enums/enums";
 
 import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
 
 type Props = {
   icon: JSX.Element;
@@ -11,7 +17,10 @@ type Props = {
 
 const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div
+      variants={childVariant}
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+    >
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
@@ -27,7 +36,7 @@ const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
       >
         <p>Learn More</p>
       </AnchorLink>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/benefit/Benefits.tsx
+++ src/components/benefit/Benefits.tsx
@@ -34,6 +34,13 @@ const benefits: Array<BenefitType> = [
   },
 ];
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -45,7 +52,17 @@ const Benefits = ({ setSelectedPage }: Props) => {
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
         {/* header */}
-        <div className="md:my-5 md:w-3/5">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1.8 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 100 },
+          }}
+          className="md:my-5 md:w-3/5"
+        >
           <HText>More Than Just Gym</HText>
 
           <p className="my-5 text-sm">
@@ -53,10 +70,16 @@ const Benefits = ({ setSelectedPage }: Props) => {
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.
           </p>
-        </div>
+        </motion.div>
         {/* benefits */}
 
-        <div className="mt-5 items-center justify-between gap-8 md:flex">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+          className="mt-5 items-center justify-between gap-8 md:flex"
+        >
           {benefits.map((benefit: BenefitType) => (
             <Benefit
               key={benefit.title}
@@ -66,7 +89,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           ))}
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
