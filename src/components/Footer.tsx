bd00305b99d0cbb997b4526ee6932de08317771d
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-07-03 03:38:00 +0530

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,13 +8,26 @@ const Footer = (props: Props) => {
       <div className="mx-auto w-5/6 justify-center gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
           <img src={logo} alt="logo" />
-          <p className="">
+          <p className="my-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
             fuga obcaecati ducimus corrupti fugit ipsam.
           </p>
+
+          <p>© Evogym All Rights Reserved.</p>
         </div>
-        <div></div>
-        <div></div>
+
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p>Ullamcorper vivamus</p>
+        </div>
+
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Hathras, INDIA</p>
+          <p>(+91)1234567890</p>
+        </div>
       </div>
     </footer>
   );
